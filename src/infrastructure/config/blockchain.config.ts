0253ea46d5197fc3e1f962ef170667c76503db2f
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-07-29 22:13:49 +0530

    blockchain-config-updated

--- src/infrastructure/config/blockchain.config.ts
+++ src/infrastructure/config/blockchain.config.ts
@@ -3,6 +3,7 @@ import { registerAs } from "@nestjs/config";
 export const BLOCKCHAIN_CONFIG = registerAs('BLOCKCHAIN', () => {
     return {
         NODE_URL: process.env['NODE_URL'],
-        PRIVATE_KEY: process.env['PRIVATE_KEY']
+        PRIVATE_KEY: process.env['PRIVATE_KEY'],
+        EXPLORER_API_KEY: process.env['EXPLORER_API_KEY']
     }
 })
