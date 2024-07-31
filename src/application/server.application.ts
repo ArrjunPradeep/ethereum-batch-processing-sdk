53d1883c6e407550b103574872a7f3b9c1c9da6c
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-07-31 11:22:27 +0530

    openAPI-documentation-configured

--- src/application/server.application.ts
+++ src/application/server.application.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { RootModule } from './di/root.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 export class ServerApplication {
   private host: string;
@@ -17,9 +18,29 @@ export class ServerApplication {
     this.host = configService.getOrThrow<string>('APP.HOST');
     this.port = configService.getOrThrow<number>('APP.PORT');
 
+    this.buildAPIDocumentaion(app);
     this.log();
 
     await app.listen(this.port, this.host);
+  }
+
+  private buildAPIDocumentaion(app: NestExpressApplication): void {
+    const title: string = "Ethereum Batch Transaction Development Kit";
+    const description: string = "API Documentation";
+    const version: string = '1.0.0';
+
+    const options: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+      .setTitle(title)
+      .setDescription(description)
+      .setVersion(version)
+      .build();
+
+    const document: OpenAPIObject = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('swagger', app, document, {
+      yamlDocumentUrl: 'swagger/yaml',
+      jsonDocumentUrl: 'swagger/json'
+    });
   }
 
   private log(): void {
