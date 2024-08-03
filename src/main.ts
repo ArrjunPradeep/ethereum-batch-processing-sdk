1299a926503b5a4d9f4cbd4df8663d5c7a01ce51
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-03 11:39:39 +0530

    detailed-comments-application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { ServerApplication } from './application/server.application';
+import { ServerApplication } from './application/server.application'; // Import the ServerApplication class
 
+// Function to run the application
 const runApplication = async (): Promise<void> => {
+  // Create a new instance of ServerApplication
   const serverApplication: ServerApplication = ServerApplication.new();
+  // Run the server application
   await serverApplication.run();
 };
 
+// Immediately-invoked function expression (IIFE) to bootstrap the application
 (async (): Promise<void> => {
-
+  // Call the runApplication function to start the server
   await runApplication();
 })();
